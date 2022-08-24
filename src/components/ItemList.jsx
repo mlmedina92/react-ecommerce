20a138fc402af8982554ddb1e330d0711e61d476
mlmedina92 <lm30540@gmail.com>
2022-08-24 09:23:30 -0300

    lista de items diseño

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -6,7 +6,17 @@ import Item from './Item';
 //capturo el data, lo inicializo en [], me llaga un array de objetos y por cada objeto genero un item con su key y le mando ese objeto q tiene toda la inf.
 const ItemList = ({data = []}) => {
   return (
-    data.map(film => <Item key={film.id} info={film}/>) // por cada elemento de data que viene de ItemListContainer y que lo llamo film, necesito q crees un componente item. el map geera una lista, entonces a cada item tengo que ponerle una prop key q es un identificador para q react sepa q item es c/u, sino para react son todos los item iguaes y si le pido agregar nuevos o borrar es problematico. el valor q le paso a key es un identificcador unic--- objeto.id. A ESTE ITEM LE TENGO Q PASAR LA DATA, LA VOY A LLAMAR INFO , le paso como valor todo el film, todo el objeto 
+    <div className="row">
+      {data.map((item) => {
+        return (
+        <div className="col">  
+        {/*  por cada elemento de data que viene de ItemListContainer y que lo llamo film, necesito q crees un componente item. el map geera una lista, entonces a cada item tengo que ponerle una prop key q es un identificador para q react sepa q item es c/u, sino para react son todos los item iguaes y si le pido agregar nuevos o borrar es problematico. el valor q le paso a key es un identificcador unic--- objeto.id. A ESTE ITEM LE TENGO Q PASAR LA DATA, LA VOY A LLAMAR INFO , le paso como valor todo el film, todo el objeto      */}
+          <Item key={item.id} info={item}/> 
+        </div>
+        )
+      })}
+      
+    </div>
   )
 }
 
